1c98a73d4e43a8550e4500e91d04658d56510316
Sergey Zhuravlev <sergey@netangels.ru>
2014-01-28 12:30:47 +0600

    Added function pam_sm_close_session()

--- pam_privtmp.c
+++ pam_privtmp.c
@@ -80,3 +80,14 @@ int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **ar
 
      return PAM_SUCCESS;
 }
+
+
+int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    (void)pamh;
+    (void)flags;
+    (void)argc;
+    (void)argv;
+
+    return PAM_SUCCESS;
+}
